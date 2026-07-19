f61438b80262b33404c4d204dda722730377ea87
sekuler <kazimmarmara16@gmail.com>
2026-07-19 15:04:04 +0300

    Fix Circle wallet API to use ES module import syntax

--- api/circle-wallet.js
+++ api/circle-wallet.js
@@ -1,6 +1,6 @@
-const { initiateDeveloperControlledWalletsClient } = require('@circle-fin/developer-controlled-wallets');
+import { initiateDeveloperControlledWalletsClient } from '@circle-fin/developer-controlled-wallets';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -45,4 +45,4 @@ module.exports = async function handler(req, res) {
     console.error('Circle wallet error:', error);
     return res.status(500).json({ error: error.message ?? 'Internal error' });
   }
-};
+}
